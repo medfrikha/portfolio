f14b6a1031b506540af67c50b673a641bec8d526
mohamed frikha <frikhamohamed95@gmail.com>
2022-08-13 12:59:10 +0100

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './index.css'
+import "./index.css";
+import "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Hello</h1>
       <button className="btn btn-success"> ok </button>
     </div>
