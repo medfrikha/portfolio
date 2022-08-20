52bb7f770be53e63a55e6ea025ed8665d19b9b42
mohamed frikha <frikhamohamed95@gmail.com>
2022-08-20 11:30:15 +0100

    Add skills details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./index.css";
 import Navbar from "./components/Navbar";
-import Contact from "./pages/Contact"
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-import Skills from "./pages/Skills"
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import SkillDetails from "./pages/SkillDetails";
 
 function App() {
   return (
     <BrowserRouter>
-        <Navbar />
-    <div className='container mt-5'>
-        <Route exact path="/" component={Home} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/contact" component={Contact}/>
-    </div>
-      </BrowserRouter>
+      <Navbar />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/skills/:id" component={SkillDetails} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Skills() {
-  const [skills, setSkills] = useState( [] );
+  const [skills, setSkills] = useState([]);
 
   useEffect(() => {
     axios
@@ -18,18 +19,20 @@ function Skills() {
   return (
     <div className="row">
       {skills.map((element) => (
-        <div key={element.id} className="card col-12 col-sm-6 col-md-4 col-lg-3 p-3">
-          <img
-            src={element.image}
-            className="card-img-top"
-            alt={`${element.name} logo`}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{element.name}</h5>
-            {/* <p className="card-text">{element.description}</p> */}
-            <a href="#" className="btn btn-primary">
-              Learn about
-            </a>
+        <div key={element.id} className="col-12 col-sm-6 col-md-4 col-lg-3 p-3">
+          <div className="card">
+            <img
+              src={element.image}
+              className="card-img-top"
+              alt={`${element.name} logo`}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{element.name}</h5>
+              {/* <p className="card-text">{element.description}</p> */}
+              <Link to={`/skills/${element.id}`} className="btn btn-primary">
+                Learn about
+              </Link>
+            </div>
           </div>
         </div>
       ))}
