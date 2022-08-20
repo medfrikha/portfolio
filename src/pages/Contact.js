97368777ff50dfa8115563e486cc24177378cd14
mohamed frikha <frikhamohamed95@gmail.com>
2022-08-20 10:03:31 +0100

    Add contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Contact() {
+  const contactInformations = {
+    email: `mohamedfrikha95@gmail.com`,
+    phoneNumber : '+216 22944 897',
+    address : {
+      city: 'Sfax',
+      country: 'Tunisia',
+      street: 'ROUTE MATAR KM2',
+      postalCode: 3023 
+    }
+  }
   return (
-    <div>Contact</div>
+    <div>
+      <p> <i class="bi bi-envelope"></i> {contactInformations.email} </p>
+      <p> <i class="bi bi-telephone"></i> {contactInformations.phoneNumber} </p>
+      <p><i class="bi bi-geo-alt"></i> {contactInformations.address.street}, {contactInformations.address.postalCode} {contactInformations.address.city}; {contactInformations.address.country} </p>
+    </div>
   )
 }
 
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,22 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Skills() {
-  const [skills, setSkills] = useState([
-    {
-      id: "1",
-      name: "HTML",
-      description:
-        "HTML is the foundation of all web pages. Without HTML, you wouldn’t be able to organize text or add images or videos to your web pages. HTML is the beginning of everything you need to know to create engaging web pages!",
-      image: "https://colabs.devmastery.tech/technologies/html.svg",
-    },
-  ]);
+  const [skills, setSkills] = useState( [] );
 
   useEffect(() => {
     axios
       .get("https://devmastery-assets.vercel.app/technologies.json")
       .then((response) => {
         setSkills(response.data);
-        console.log(skills);
       })
       .catch((error) => {
         console.log(error);
@@ -27,7 +18,7 @@ function Skills() {
   return (
     <div className="row">
       {skills.map((element) => (
-        <div key={element.id} className="col-12 col-sm-6 col-md-4 col-lg-3 p-3">
+        <div key={element.id} className="card col-12 col-sm-6 col-md-4 col-lg-3 p-3">
           <img
             src={element.image}
             className="card-img-top"
@@ -35,7 +26,7 @@ function Skills() {
           />
           <div className="card-body">
             <h5 className="card-title">{element.name}</h5>
-            <p className="card-text">{element.description}</p>
+            {/* <p className="card-text">{element.description}</p> */}
             <a href="#" className="btn btn-primary">
               Learn about
             </a>
